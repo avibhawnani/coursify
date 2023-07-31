31bea6a2e35267f02cc8359a4a1f75ae33aceeab
avibhawnani <avibhawnani.in@gmail.com>
2023-07-31 16:36:32 +0530

    Update in paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,97 +1,110 @@
 import { catchAsyncError } from "../middleware/catchAsyncError.js";
-import { User } from "../models/UserModel.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { instance } from "../server.js";
-import crypto from "crypto";
+import { User } from "../models/UserModel.js";
 import { Payment } from "../models/PaymentModel.js";
+import crypto from "crypto";
 
-export const buySubscription = catchAsyncError(async(req,res,next)=>{
+// Buy Subscription
+export const buySubscription = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  if (user.role === "admin")
+    return next(
+      new ErrorHandler("Admin doesnot have to buy Subscription", 400)
+    );
 
-    const user = await User.findById(req.user._id);
+  const plan_id = process.env.PLAN_ID;
+  const subscription = await instance.subscriptions.create({
+    plan_id,
+    customer_notify: 1,
+    total_count: 12,
+  });
 
-    if(user.role === "admin") return next(new ErrorHandler("Admin cannot buy subscription",400));
+  user.subscription.id = subscription.id;
+  user.subscription.status = subscription.status;
 
+  await user.save();
 
-    const plan_id = process.env.PLAN_ID || "plan_MF9aFCrFRRr8Mw";
-    const subscription = await instance.subscriptions.create({
-        plan_id: plan_id,
-        customer_notify: 1,
-        total_count: 12,
-    });
-    user.subscription.id = subscription.id;
-    user.subscription.status = subscription.status;
-    await user.save();
-
-    res.status(201).json({
-        success:true,
-        subscriptionId:subscription.id,
-    });
+  res.status(201).json({
+    success: true,
+    subscriptionId: subscription.id,
+  });
 });
 
-export const paymentverification = catchAsyncError(async(req,res,next)=>{
+// Payment Verification and Save reference in Database
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+  const { razorpay_payment_id, razorpay_subscription_id, razorpay_signature } =
+    req.body;
 
-    const {razorpay_payment_id,razorpay_subscription_id,razorpay_signature} = req.body;
-    const user = await User.findById(req.user._id);
-    const subscriptionId = user.subscription.id;
+  const user = await User.findById(req.user._id);
 
-    if(user.role === "admin") return next(new ErrorHandler("Admin cannot buy subscription",400));
-    const generated_signature = crypto.createHmac("sha256",process.env.RAZORPAY_API_SECRET)
-                                .update(razorpay_payment_id+"|"+subscriptionId,"utf-8").digest("hex");
-                
-    const isAuthentic = generated_signature === razorpay_signature;
-    if(!isAuthentic) 
-        return res.redirect(`${process.env.FRONTEND_URL}/paymentfailed`);
+  const subscription_id = user.subscription.id;
 
-    await Payment.create({razorpay_payment_id,
-                          razorpay_subscription_id,
-                          razorpay_signature});
-    user.subscription.status = "active";
-    await user.save();
-    
+  const generated_signature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
 
-    res.redirect(
-        `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
-      );
+  const isAuthentic = generated_signature === razorpay_signature;
+  if (!isAuthentic)
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
+
+  // Database comes here
+  await Payment.create({
+    razorpay_payment_id,
+    razorpay_subscription_id,
+    razorpay_signature,
+  });
+
+  user.subscription.status = "active";
+
+  await user.save();
+
+  res.redirect(
+    `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
+  );
 });
 
 // Get RazorPay Key
 export const getRazorPayKey = catchAsyncError(async (req, res, next) => {
-    res.status(200).json({
-      success: true,
-      key: process.env.RAZORPAY_API_KEY,
-    });
+  res.status(200).json({
+    success: true,
+    key: process.env.RAZORPAY_API_KEY,
+  });
 });
 
 // Cancel Subscription
 export const cancelSubscription = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  const subscriptionId = user.subscription.id;
 
-    const user = await User.findById(req.user._id);
-    const subscriptionId = user.subscription.id;
-    let refund = false;
+  let refund = false;
 
-    await instance.subscriptions.cancel(subscriptionId);
+  await instance.subscriptions.cancel(subscriptionId);
 
-    const payment = await Payment.findOne({
-        razorpay_payment_id:subscriptionId,
-    });
-    const gap = Date.now() - payment.createdAt;
-    const refundTime = process.env.REFUND_DAYS * 24*60*60*1000;
+  const payment = await Payment.findOne({
+    razorpay_subscription_id: subscriptionId,
+  });
 
-    if(refundTime > gap){ 
-        await instance.payments.refund(payment.razorpay_payment_id);
-        refund = true;
-    }
+  const gap = Date.now() - payment.createdAt;
 
-    await payment.deleteOne();
-    // user.subscription.id = undefined;
-    // user.subscription.status = undefined;
-    user.subscription = undefined;
-    await user.save();
+  const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
 
-    res.status(200).json({
-      success: true,
-      message: refund
+  if (refundTime > gap) {
+    await instance.payments.refund(payment.razorpay_payment_id);
+    refund = true;
+  }
+
+  await payment.deleteOne();
+  //   user.subscription.id = undefined;
+  //   user.subscription.status = undefined;
+  user.subscription = undefined;
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: refund
       ? `${user.name}: Your Subscription is now cancelled, You will recieve full refund within 7 days`
       : `Subscription Cancelled, Now refund initiated as subscription was cancelled after 7 days.`,
-    });
   });
+});
